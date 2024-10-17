fd4256732bcbea274468f8e914d5969af3778757
oswyndel <mishabatashev1@gmail.com>
2024-10-17 04:16:38 +0300

    new benchs

--- benchs/bench_test.go
+++ benchs/bench_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/RbPyer/Gofetch/internal/models"
 	p "github.com/RbPyer/Gofetch/internal/parsers"
 	"github.com/RbPyer/Gofetch/internal/presentator"
+	"sync"
 )
 
 func fetch() {
@@ -39,8 +40,74 @@ func fetch() {
 	presentator.Present(response)
 }
 
-func BenchmarkNeofetch(b *testing.B) {
+func routinesFetch() {
+	response := &models.Response{}
+	wg := new(sync.WaitGroup)
+	wg.Add(5)
+
+	go func() {
+		if err := p.GetDiskInfo(response); err != nil {
+			log.Fatalf("Some errors with getting disk info: %s", err.Error())
+		}
+		wg.Done()
+	}()
+
+	go func() {
+		if err := p.GetGPUInfo(response); err != nil {
+			log.Fatalf("Some errors with getting disk info: %s", err.Error())
+		}
+		wg.Done()
+	}()
+
+	go func() {
+		if err := p.GetCPUInfo(response); err != nil {
+			log.Fatalf("Some errors with getting cpu info: %s", err.Error())
+		}
+		wg.Done()
+	}()
+
+	go func() {
+		if err := p.GetRAMInfo(response); err != nil {
+			log.Fatalf("Some errors with getting ram info: %s", err.Error())
+		}
+		wg.Done()
+	}()
+
+	go func() {
+		if err := p.GetOsVersion(response); err != nil {
+			log.Fatalf("Some errors with getting OS version: %s", err.Error())
+		}
+		wg.Done()
+	}()
+
+	if err := p.GetUserInfo(response); err != nil {
+		log.Fatalf("Some errors with getting user: %s", err.Error())
+	}
+
+	if err := p.GetKernelVersion(response); err != nil {
+		log.Fatalf("Some errors with getting kernel version: %s", err.Error())
+	}
+
+
+	if err := p.GetUptime(response); err != nil {
+		log.Fatalf("Some errors with getting system uptime: %s", err.Error())
+	}
+
+	p.GetShell(response)
+	wg.Wait()
+
+	presentator.Present(response)
+}
+
+func BenchmarkFetch(b *testing.B) {
     for i := 0; i < b.N; i++ {
         fetch()
+    }
+}
+
+
+func BenchmarkRoutinesFetch(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+        routinesFetch()
     }
 }
