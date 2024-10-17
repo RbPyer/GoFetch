f3c64a221630d129fb6bbcdce13e25df289bce34
oswyndel <mishabatashev1@gmail.com>
2024-10-17 05:38:13 +0300

    some optimizations

--- cmd/main.go
+++ cmd/main.go
@@ -5,65 +5,36 @@ import (
 
 	"github.com/RbPyer/Gofetch/internal/models"
 	p "github.com/RbPyer/Gofetch/internal/parsers"
-	"sync"
 	"github.com/RbPyer/Gofetch/internal/presentator"
 )
 
 func main() {
-	response := &models.Response{}
-	wg := new(sync.WaitGroup)
-	wg.Add(5)
-
-	go func() {
-		if err := p.GetDiskInfo(response); err != nil {
-			log.Fatalf("Some errors with getting disk info: %s", err.Error())
-		}
-		wg.Done()
-	}()
-
-	go func() {
-		if err := p.GetGPUInfo(response); err != nil {
-			log.Fatalf("Some errors with getting disk info: %s", err.Error())
-		}
-		wg.Done()
-	}()
-
-	go func() {
-		if err := p.GetCPUInfo(response); err != nil {
-			log.Fatalf("Some errors with getting cpu info: %s", err.Error())
-		}
-		wg.Done()
-	}()
-
-	go func() {
-		if err := p.GetRAMInfo(response); err != nil {
-			log.Fatalf("Some errors with getting ram info: %s", err.Error())
-		}
-		wg.Done()
-	}()
-
-	go func() {
-		if err := p.GetOsVersion(response); err != nil {
-			log.Fatalf("Some errors with getting OS version: %s", err.Error())
-		}
-		wg.Done()
-	}()
-
-	if err := p.GetUserInfo(response); err != nil {
+	response := models.New()
+	if err := p.GetUserInfo(&response); err != nil {
 		log.Fatalf("Some errors with getting user: %s", err.Error())
 	}
-
-	if err := p.GetKernelVersion(response); err != nil {
+	if err := p.GetOsVersion(&response); err != nil {
+		log.Fatalf("Some errors with getting OS version: %s", err.Error())
+	}
+	if err := p.GetKernelVersion(&response); err != nil {
 		log.Fatalf("Some errors with getting kernel version: %s", err.Error())
 	}
-
-
-	if err := p.GetUptime(response); err != nil {
+	if err := p.GetUptime(&response); err != nil {
 		log.Fatalf("Some errors with getting system uptime: %s", err.Error())
 	}
+	if err := p.GetCPUInfo(&response); err != nil {
+		log.Fatalf("Some errors with getting cpu info: %s", err.Error())
+	}
+	if err := p.GetRAMInfo(&response); err != nil {
+		log.Fatalf("Some errors with getting ram info: %s", err.Error())
+	}
+	if err := p.GetDiskInfo(&response); err != nil {
+		log.Fatalf("Some errors with getting disk info: %s", err.Error())
+	}
+	if err := p.GetGPUInfo(&response); err != nil {
+		log.Fatalf("Some errors with getting disk info: %s", err.Error())
+	}
+	p.GetShell(&response)
 
-	p.GetShell(response)
-	wg.Wait()
-
-	presentator.Present(response)
+	presentator.Present(&response)
 }
